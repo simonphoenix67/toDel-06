45ef0d9f440eee8bc87e67860181a7ed08ed6a19
simonphoenix67 <51173301+simonphoenix67@users.noreply.github.com>
2023-01-29 21:24:25 +0200

    Initial commit

---
+++ src/js/02-video.js
@@ -0,0 +1,16 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate',  throttle( event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+    }, 1000)
+    );
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
